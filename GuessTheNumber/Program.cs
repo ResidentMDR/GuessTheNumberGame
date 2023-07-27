a4096c4cc98919af791769cce1bffc7af1895e58
marcin <marcin220798@gmail.com>
2023-07-27 13:28:27 +0200

    Create logic for guessing

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GuessTheNumber;
+
+Console.WriteLine("Welcome to the Guess The Number Game, input your number from range 1-100");
+
+var rng = new RandomNumberService();
+int guess = 0;
+
+while (guess != rng.CorrectNumber)
+{
+    var stringGuess = Console.ReadLine();
+    guess = Convert.ToInt32(stringGuess);
+
+    if (guess < rng.CorrectNumber)
+    {
+        Console.WriteLine("The correct number is higher...");
+    }
+    else if (guess > rng.CorrectNumber)
+    {
+        Console.WriteLine("The correct number is lower...");
+    }
+}
+
+if (guess == rng.CorrectNumber)
+{
+    Console.WriteLine($"Congratulations! You just guessed the correct number: {rng.CorrectNumber.ToString()}");
+}
+
+
---
+++ GuessTheNumber/RandomNumberService.cs
@@ -0,0 +1,19 @@
+﻿namespace GuessTheNumber
+{
+    public class RandomNumberService
+    {
+        public RandomNumberService()
+        {
+            GetNumberAtRandom();
+        }
+
+        public int CorrectNumber { get; set; }
+
+
+        private void GetNumberAtRandom()
+        {
+            Random rng = new Random();
+            CorrectNumber = rng.Next(1, 100);
+        }
+    }
+}
